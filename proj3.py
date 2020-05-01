3d7a812b31974edee85161eaa6fc47b7d3ade9aa
Madhu Ratnakar <madhu@localhost.localdomain>
2020-05-01 18:20:42 -0400

    printing game greeting

--- proj3.py
+++ proj3.py
@@ -1,1 +1,18 @@
-print ('Hello World')
+#!/usr/bin/python3
+# Hangman game
+
+import random
+
+def pickWord():
+    return words[random.randint(0, len(words) - 1)]
+
+
+words = '''key virginia technology'''.split()
+
+print('Welcome to Hangman !')
+
+word = list(pickWord())
+
+result = list('_' * len(word))
+
+print('The word is: ', result)
